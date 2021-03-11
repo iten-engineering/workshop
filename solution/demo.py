96d76a7e5eb269e10a14a1bfda7a63dad6506e6b
Tom <tom.iten@surfmachine.ch>
2021-03-11 11:31:47 +0100

    work

--- solution/demo.py
+++ solution/demo.py
@@ -1,10 +1,3 @@
 
-names = ["Peter", "Jane", "Fred"]
-ages  = [31, 35, 4]
-
-for t in zip(names, ages):
-    print(t[1])
-
-for name, age in zip(names, ages):
-    print("{} ist {} Jahre alt".format(name, age))
-
+while True:
+    print("Hello")
