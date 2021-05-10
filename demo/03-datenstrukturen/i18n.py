27ebb83e70bc51e51dc6bcbb5cbfaeb57a588094
Tom <tom.iten@surfmachine.ch>
2021-05-10 16:20:27 +0200

    work

--- demo/03-datenstrukturen/i18n.py
+++ demo/03-datenstrukturen/i18n.py
@@ -7,9 +7,13 @@ i18n = {
 }
 
 name = input("Geben Sie bitte Ihren Namen ein: ")
-key  = input("Sprache auswählen [de,fr,it,en]: ")
+# key  = input("Sprache auswählen [de,fr,it,en]: ")
 
-key = key if key in i18n.keys() else "en"
+validation = False
+while validation == False:
+    key  = input("Sprache auswählen [de,fr,it,en]: ")
+    validation = True if key in i18n.keys() else False
+
 
 greeting = i18n[key]
 print(greeting)
