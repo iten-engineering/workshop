39dbc2aa830ad5d65d90fd0996664392615f4955
Tom <tom.iten@surfmachine.ch>
2021-05-11 10:43:38 +0200

    work

--- demo/04-klassen/mathutil.py
+++ demo/04-klassen/mathutil.py
@@ -9,7 +9,12 @@ class MathUtil:
             return b
         return c    
 
-        
+    @staticmethod
+    def max(a, b, c):
+        if a > b and a > c:
+            return a
+        if b > c:
+            return b
+        return c   
 
 
-print (MathUtil.min(1,2,-3))
--- demo/04-klassen/mathutil_test.py
+++ demo/04-klassen/mathutil_test.py
@@ -12,6 +12,11 @@ class MathUtilTest(unittest.TestCase):
     def test_min2(self):
         self.assertEqual(3, MathUtil.min(49, 3, 14))
 
+    def test_max(self):
+        self.assertEqual(49, MathUtil.max(49, 3, 14))
+
+    def test_max2(self):
+        self.assertEqual(300, MathUtil.max(49, 300, 14))
 
 
 if __name__ == "__main__":
