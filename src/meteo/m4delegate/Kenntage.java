c71dfc9b7806d658229bfe7e88eb47f70396ebb2
iten-engineering <tom.iten@surfmachine.ch>
2021-08-11 08:57:56 +0200

    work

---
+++ src/meteo/m4delegate/Kenntage.java
@@ -0,0 +1,14 @@
+package meteo.m4delegate;
+
+public interface Kenntage {
+
+	public boolean isWuestenTag();
+	public boolean isHeisserTag();
+	public boolean isSommerTag();
+	public boolean isEisTag();
+	public boolean isTropenNacht();
+	public boolean isVegetationsTag();
+	public boolean isFrostTag();
+	public boolean isHeizTag();
+	
+}
---
+++ src/meteo/m4delegate/Temperaturen.java
@@ -0,0 +1,10 @@
+package meteo.m4delegate;
+
+public interface Temperaturen {
+
+	public double[] getTemperaturen();
+	public double getMittelWert();
+	public double getMin();
+	public double getMax();
+	
+}
