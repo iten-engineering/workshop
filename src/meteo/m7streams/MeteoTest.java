e0ec21ad4e62fe11be2b7632e4c20b69dc38b236
iten-engineering <tom.iten@surfmachine.ch>
2021-08-13 11:14:08 +0200

    work

---
+++ src/meteo/m7streams/MeteoTest.java
@@ -0,0 +1,65 @@
+package meteo.m7streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MeteoTest {
+
+	public static final double[] TEST_DATA = { -8.1, -4.5, -2.1, -1.3, -0.1, -2.2, -4.1, -6.3 };
+	public static final double TOLERANCE = 0.001;
+
+	@Test
+	public void testTemperaturen() {
+		Meteo meteo = new Meteo(TEST_DATA);
+
+		Assert.assertEquals(-8.1, meteo.getMin(), TOLERANCE);
+		Assert.assertEquals(-0.1, meteo.getMax(), TOLERANCE);
+		Assert.assertEquals(-3.5875, meteo.getMittelWert(), TOLERANCE);
+	}
+
+	@Test
+	public void testKenntage() {
+		Meteo meteo = new Meteo(TEST_DATA);
+
+		Assert.assertTrue(meteo.isEisTag());
+		Assert.assertTrue(meteo.isFrostTag());
+		Assert.assertFalse(meteo.isHeisserTag());
+		Assert.assertTrue(meteo.isHeizTag());
+		Assert.assertFalse(meteo.isSommerTag());
+		Assert.assertFalse(meteo.isTropenNacht());
+		Assert.assertFalse(meteo.isVegetationsTag());
+		Assert.assertFalse(meteo.isWuestenTag());
+	}
+
+	@Test
+	public void testKenntageEnum() {
+		Meteo meteo = new Meteo(TEST_DATA);
+
+		List<Kenntag> kenntage = meteo.getKenntage();
+
+		Assert.assertEquals(3, kenntage.size());
+		Assert.assertTrue(kenntage.contains(Kenntag.EIS_TAG));
+		Assert.assertTrue(kenntage.contains(Kenntag.FROST_TAG));
+		Assert.assertTrue(kenntage.contains(Kenntag.HEIZ_TAG));
+	}
+
+	@Test
+	public void testTestData() {
+		String testRecord = "-8.1, -3.5875, -0.1 : -8.1, -4.5, -2.1, -1.3, -0.1, -2.2, -4.1, -6.3";
+		MeteoTestData testData = new MeteoTestData(testRecord);
+
+		
+		Assert.assertEquals(-8.1, testData.getExpectedMin(), TOLERANCE);
+		Assert.assertEquals(-3.5875, testData.getExpectedMed(), TOLERANCE);
+		Assert.assertEquals(-0.1, testData.getExpectedMax(), TOLERANCE);
+		
+		Assert.assertEquals(8, testData.getTemperaturen().length);
+		Assert.assertEquals(-8.1, testData.getTemperaturen()[0], TOLERANCE);
+		Assert.assertEquals(-1.3, testData.getTemperaturen()[3], TOLERANCE);
+		Assert.assertEquals(-6.3, testData.getTemperaturen()[7], TOLERANCE);
+	}
+
+} // end
---
+++ src/meteo/m7streams/Temperaturen.java
@@ -0,0 +1,13 @@
+package meteo.m7streams;
+
+public interface Temperaturen {
+
+	public double[] getTemperaturen();
+	
+	public double getMittelWert();
+
+	public double getMin();
+
+	public double getMax();
+
+}
