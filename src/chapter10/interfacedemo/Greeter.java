89bcddc011d864bab33840a5299e245ad17dea0b
Tom <tom.iten@surfmachine.ch>
2021-08-19 12:23:54 +0200

    work

--- src/chapter10/interfacedemo/Greeter.java
+++
@@ -1,9 +0,0 @@
-package chapter10.interfacedemo;
-
-public interface Greeter {
-
-	public void sayHello();
-	
-	public void sayHello(String name);
-	
-}
--- src/designpattern/MyLogger.java
+++
@@ -1,7 +0,0 @@
-package designpattern;
-
-public interface MyLogger {
-	public void debug(String message);
-	public void warn(String message);
-	public void error(String message);
-}
