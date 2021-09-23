483477bfc846fa6b3bf003e41fb8caee06e01a7b
Tom <tom.iten@surfmachine.ch>
2021-09-23 09:48:58 +0200

    work

---
+++ lab/02-basic/rechteck.py
@@ -0,0 +1,9 @@
+
+laenge = 10
+breite = 5
+umfang = 2 * (laenge + breite)
+flaeche = laenge * breite
+
+print("Rechteck mit Länge", laenge, "und Breite", breite)
+print("- Umfang =", umfang)
+print("- Fläche =", flaeche)
