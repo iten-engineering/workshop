d30168681d8c4b642aec35a468f997c916566498
Tom <tom.iten@surfmachine.ch>
2021-09-24 16:55:19 +0200

    work

--- lab/02-basic/rechteck.py
+++
@@ -1,9 +0,0 @@
-
-laenge = 10
-breite = 5
-umfang = 2 * (laenge + breite)
-flaeche = laenge * breite
-
-print("Rechteck mit Länge", laenge, "und Breite", breite)
-print("- Umfang =", umfang)
-print("- Fläche =", flaeche)
--- lab/09-apps/bookservice/run.bat
+++
@@ -1,3 +0,0 @@
-set FLASK_APP=bookservice.py
-
-D:\dev\software\miniconda3\python.exe -m flask run
