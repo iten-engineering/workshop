99a2eaa75c937be6862e696a5e71fa018f464533
Tom <tom.iten@surfmachine.ch>
2021-11-22 12:48:51 +0100

    work

--- lab/05-file/umsatz.py
+++ lab/05-file/umsatz.py
@@ -1,8 +1,30 @@
+# -------------------------------------------------------------------------------------------------
+# Excel Beispiel mit Pandas und Matplotlib
+#
+# Referenzen:
+# - https://www.journaldev.com/33306/pandas-read_excel-reading-excel-file-in-python
+# - https://pythonspot.com/matplotlib-bar-chart/ 
+# -------------------------------------------------------------------------------------------------
+
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_excel('lab/05-file/umsatz.xlsx') 
 print(df)
 
-for row in df.iterrows():
-    # print(row)
-    print(row['Monat'], row['Umsatz'])
+monat = df['Monat'].tolist()
+umsatz= df['Umsatz'].tolist()
+
+print(monat)
+print(umsatz)
+
+xpos = np.arange(len(monat))
+plt.bar(xpos, umsatz, align='center', alpha=0.5)
+plt.xticks(xpos, monat)
+
+plt.title('Umsätze Januar bis Dezember')
+plt.xlabel('Monat')
+plt.ylabel('Umsatz')
+
+plt.show()
