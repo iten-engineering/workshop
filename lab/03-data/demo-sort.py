72c18129a796563b4c4855dc6fe0a1c6a60a490d
Tom <tom.iten@surfmachine.ch>
2021-11-22 17:16:16 +0100

    work

---
+++ lab/03-data/demo-sort.py
@@ -0,0 +1,11 @@
+items = [1, 2, 0, 7]
+
+print(items)
+
+# Sort items "in place", does not return the sorted list
+items.sort()                    
+print(items)
+
+items.sort(reverse=True)
+print(items)
+
