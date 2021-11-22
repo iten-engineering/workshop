da72e361e9015a18e265134fed36e489ed95b655
Tom <tom.iten@surfmachine.ch>
2021-11-22 09:59:18 +0100

    work

---
+++ lab/02-basics/fahrenheit.py
@@ -0,0 +1,10 @@
+
+value = input("Temperatur in Fahrenheit = ")
+# print(value)
+# print(type(value))
+
+fahrenheit = float(value)
+celsius = 5 * (fahrenheit-32) / 9
+
+print("Fahrenheit   =", fahrenheit)
+print("Grad Celsius =", celsius)
