9e90a319b8726ca34cc41d68268edc23fd9e1023
Tom <tom.iten@surfmachine.ch>
2021-11-22 14:36:33 +0100

    work

---
+++ lab/02-basics/linear.py
@@ -0,0 +1,6 @@
+
+f = lambda x: x * 0.5 + 1
+
+for x in [-3,-2,-1,0,1,2,3]:
+    y = f(x)
+    print("x:", x, "y:", y)
---
+++ lab/02-basics/square.py
@@ -0,0 +1,5 @@
+
+square = lambda x: x**2
+
+for i in range(1,5):
+    print(square(i))
