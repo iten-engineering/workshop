e454b1223b84e0397bdeb89959c9275afe5781c4
Tom <tom.iten@surfmachine.ch>
2021-11-22 16:18:47 +0100

    work

---
+++ lab/artikel.py
@@ -0,0 +1,9 @@
+
+articles = {
+    11: {'name': 'Bildschirm Belinea X3', 'price': 499.5},
+    12: {'name': 'PC Tastatur Swiss German', 'price': 35.0},
+    13: {'name': 'Logitec Maus', 'price': 17.25},
+    14: {'name': 'USB Hub', 'price': 25.7},
+    15: {'name': 'Lautsprecher X66-12', 'price': 87.9}
+}
+
