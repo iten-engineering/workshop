d9a3b0ec912dd86f754aaadb6b000189f0893db9
Tom <tom.iten@surfmachine.ch>
2021-11-22 14:39:18 +0100

    work

--- lab/02-basics/linear.py
+++ lab/02-basics/linear.py
@@ -1,6 +1,9 @@
 
 f = lambda x: x * 0.5 + 1
 
+def f2(x):
+    y = x * 0.5 + 1
+    return y
+
 for x in [-3,-2,-1,0,1,2,3]:
-    y = f(x)
-    print("x:", x, "y:", y)
+    print("x:", x, "y:", f2(x))
