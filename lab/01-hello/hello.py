1f965d1fd55f9b22dfabc7c23b75c7abd5dbb6b6
Tom <tom.iten@surfmachine.ch>
2022-08-10 09:30:53 +0200

    work

---
+++ lab/01-hello/hello.py
@@ -0,0 +1,10 @@
+
+print("Hello World.")
+
+name = input("Geben sie ihren Namen ein:")
+print("Hello", name, sep="-")
+
+
+https://github.com/iten-engineering/python
+
+https://github.com/iten-engineering/workshop 
