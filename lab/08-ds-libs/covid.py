94b02153fe2c1202719a2c7f5dd6e2ec90c17728
Tom <tom.iten@surfmachine.ch>
2022-08-12 13:19:34 +0200

    work

--- lab/08-ds-libs/covid.py
+++ lab/08-ds-libs/covid.py
@@ -1,9 +1,27 @@
 import json
+import matplotlib.pyplot as plt
+import numpy as np
 
 file = "lab/08-ds-libs/COVID19Cases_geoRegion.json"
 
 with open(file) as f:
     records = json.load(f)
 
+x = []
+y = []
+
 for rec in records:
-    print(rec['geoRegion'], rec['datum'], rec['entries'])
+    # skip other regions
+    if rec['geoRegion'] != "CH":
+        continue
+    # continue processing
+    x.append(rec['datum'])
+    y.append(rec['entries'])
+
+plt.figure()
+plt.plot(x,y)
+plt.xticks(np.arange(0, len(x)+1,100))
+plt.show()
+
+
+
